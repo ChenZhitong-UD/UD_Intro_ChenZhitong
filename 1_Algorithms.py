ceebd338f931f7b74b44283694847a5221d695e3
ChenZhitong-UD <ucbvzc7@ucl.ac.uk>
2024-10-16 11:34:58 +0100

    Add files via upload

--- 1_Algorithms.py
+++ 1_Algorithms.py
@@ -1,38 +1,3 @@
-print("Introduction to Programming")
-
-name = "ZHITONG CHEN"
-email = "ucbvzc7@ucl.ac.uk"
-age = "23"
-print(name + '\n' + email + '\n' + age)
-
-name = "ZHITONG CHEN"
-email = "ucbvzc7@ucl.ac.uk"
-age = "23"
-print(f"name: {name} \nemail: {email} \nage: {age}")
-
-print(0)
-print(178)
-print(-21)
-print(2938/49)
-print(436*9948)
-print(12**20)
-
-from math import *
-print(sin(radians(200)))
-print(cos(radians(100)))
-print(tan(pi/4))
-
-firstname = input()
-lastname = input()
-print(firstname + ' ' + lastname)
-
-import random
-random_number = random.randint(1,20)
-guessed_number = 0
-while not random_number == guessed_number:
-    guessed_number = int(input())
-print('Congratulations, you guessed it!')
-
 mylist = []
 for n in range (20):
     mylist.append(n*5)
@@ -47,35 +12,49 @@ def function(input):
     return(x)
 print(function(20))
 
-dic = {'name':'leica', 'type':'film camera', 'location':'german', 'year':1955, 'model':'iiif', 'lens':'elmar'}
-print(dic)
+brand = {'name':'leica', 'type':'film camera', 'location':'german', 'year':1955, 'model':'iiif', 'lens':'elmar'}
+print(brand)
 
 def adic(c,z):
-    for n in z.keys():
-        if c == n:
+    for n in c.keys():
+        if z == n:
             return True
     return False
+print(adic(brand,'name'))
 
-def swapDictionary(d):
-    newDict = {}
-    for k in d.keys():
-        newDict[d[k]]=k
-    return newDict
+def adic(c,z):
+    for n in c.values():
+        if z == n:
+            return True
+    return False
+print(adic(brand,'leica'))
 
-swapDictionary(neueNationalGalerie)
+def adic(c,z):
+    for n in c.items():
+        if z == n:
+            return True
+    return False
+print(adic(brand,'leica'))
+
+def swapdic(c):
+    newdic = {}
+    for z in c.keys():
+        newdic[c[z]]=z
+    return newdic
+print(swapdic(brand))
 
 dict1 = {'a':1, 'b':2, 'c':3, 'd':2}
-def swapDictionary(d):
-    newDict = {}
-    for k in d.keys():
-        if inDictionary(newDict,d[k]):
-            cv = newDict[d[k]]
-            nv = [cv, k]
-            newDict[d[k]]=nv
+def swapdic(c):
+    newdic = {}
+    for z in c.keys():
+        if adic(newdic,c[z]):
+            cv = newdic[c[z]]
+            nv = [cv, z]
+            newdic[c[z]]=nv
         else:
-            newDict[d[k]]=k
-    return newDict
-swapDictionary(dict1)
+            newdic[c[z]]=z
+    return newdic
+print(swapdic(dict1))
 
 def fib(n):
     x = {}
